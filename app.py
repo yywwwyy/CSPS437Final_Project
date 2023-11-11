37e77be61e827b374ba6807b54f27c66daa836c5
yywwwyy <3173231486@qq.com>
2023-11-11 15:21:24 -0500

    new templates for web app

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+
+
+@app.route('/')
+def home():
+    users = User.query.all()
+    return render_template('index.html', users=users)
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    username = request.form['username']
+    new_user = User(username=username)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route('/delete/<int:id>')
+def delete_user(id):
+    user_to_delete = User.query.get_or_404(id)
+    db.session.delete(user_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
